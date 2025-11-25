0069329db109ca4b40a744d38cc797c40f8bb01c
Xiaoji Zheng <student.xiaoji@gmailcom>
2025-11-25 23:00:41 +0800

    exp for cmd&usage + wm_action and critic_gamma

---
+++ tools/exp_scripts/exp_integration_cmd_usage+wm_action_2.sh
@@ -0,0 +1,3 @@
+sleep 5
+cd ~/projects/CoIRL-AD
+./tools/nusc_my_train.sh coirl/exp/15_coirl_v2+model_uncertainty_weight_1e-4+wm_action_mean+cmd_usage_before_planning 8 coirl_v2_rebuttal_exp/15+model_uncertainty_weight_1e-4+wm_action_mean+cmd_usage_before_planning
